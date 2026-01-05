d9444295de7b736822780e214f8fa5f98cbce229
zwsh-cmd <ziwush@gmail.com>
2026-01-05 17:24:04 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'script-roule-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './api.js',
+  './data.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
